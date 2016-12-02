7d5c8a83948e632322b4f7b9218a996df5c1e0b6
DanTNC <fad11204@yahoo.com.tw>
2016-12-02 21:00:46 +0800

    Update MSOEctr.js

--- js/MSOEctr.js
+++ js/MSOEctr.js
@@ -1,26 +1,26 @@
-var abcstr="$";
+var abcstr="$"; //abcstring
 var Tstate=1; //0:A, 1:A  2:a  3:a'
 var Dstate=5; //Mn, n=0~8. n=5 for N=1, n+1=>N*2, n-1=>N/2. 1n=N*(1+1/2), 2n=N*(1+1/2+1/4)... and so on
-var CrtPos=0;
+var CrtPos=0; //current position
 var abcjs=window.ABCJS;
 
 var ttlstr="";
-var chgttl = (a) => {
+var chgttl = (a) => {//update title
   ttlstr=a.value;
   print();
 };
 var tmpstr="";
-var chgtmp = (a) => {
+var chgtmp = (a) => {//update tempo
   if(a.value.length==2) a.value=a.value[0]+"/"+a.value[1];
   tmpstr=a.value;
   print();
 }
 
-var print = () => {
-  abcjs.renderAbc('boo',"T: "+ttlstr+"\nM: "+tmpstr+"\nL: 1/4\n|"+rmsmb(abcstr),{},{add_classes:true, editable:true, listener:{highlight:(abcElem)=>{
+var print = () => {//output svg
+  abcjs.renderAbc('boo',"T: "+ttlstr+"\nM: "+tmpstr+"\nL: 1/4\n|"+rmsmb(abcstr),{},{add_classes:true, editable:true, listener:{highlight:(abcElem)=>{//update CrtPos when note is clicked
       console.log(abcElem.startChar);
       var offset=abcElem.startChar-15-ttlstr.length-tmpstr.length;
-      var ignsmbs=["$","#"];
+      var ignsmbs=["$","#","*"];//symbols that won't be in the final abcstring
       for(var i=0;i<abcstr.length;i++){
         if(!ignsmbs.includes(abcstr[i])){
           if(offset!=1){
@@ -28,7 +28,7 @@ var print = () => {
           }else if(abcstr[i]!="["){
             CrtPos=i-1;
             return;
-          }else{
+          }else{//for chord
             CrtPos=i-2;
             return;
           }
@@ -40,21 +40,21 @@ var print = () => {
 
 
 var mvpos = (md) => {
-	if(md==0){
+	if(md==0){//0: move to the right note (not change if on the first note)
 		for(var i=CrtPos-1;i>=0;i--){
 			if(abcstr[i]=="$"){
 				return i;
 			}
 		}
 	}
-	if(md==1){
+	if(md==1){//1: move to the left note(not change if on the last note)
 		for(var i=CrtPos+1;i<=abcstr.length;i++){
 			if(abcstr[i]=="$"){
 				return i;
 			}
 		}
 	}
-  if(md==2){
+  if(md==2){//2: move to the last note of the upper line(not change if on the first line)
     for(var i=CrtPos-1;i>=0;i--){
       if(abcstr[i]=="\n"){
         for(var j=i-1;j>=0;j--){
@@ -65,7 +65,7 @@ var mvpos = (md) => {
       }
     }
   }
-  if(md==3){
+  if(md==3){//3: move to the first note of the lower line(not change if on the last line)
     for(var i=CrtPos+1;i<=abcstr.length;i++){
       if(abcstr[i]=="\n"){
         for(var j=i+1;j<=abcstr.length;j++){
@@ -79,10 +79,10 @@ var mvpos = (md) => {
 	return CrtPos;
 };
 
-var numtostr = (num) => {
-  if(!Number.isInteger(num)){
-    var Dnmntr=0;
-    var Nmrtr=0;
+var numtostr = (num) => {//transform a float to a string in fraction form(for duration of a note)
+  if(!Number.isInteger(num)){//if num is a integer, return it and it will be transformed to string automatically
+    var Dnmntr=0;//denominator
+    var Nmrtr=0;//numerator
     do{
       num*=2;
       Dnmntr++;
@@ -91,15 +91,15 @@ var numtostr = (num) => {
     Nmrtr=num;
     num=Nmrtr+"/"+Dnmntr;
   }
-  if(num===1){
+  if(num===1){//1 doesn't need to be noted in abcstring
     num="";
   }
   return num;
 };
 
-var toabcnote = (ch) => {
-  if(ch!="z"){
-    switch(Tstate){
+var toabcnote = (ch) => {//generate a string for a note in ABC format
+  if(ch!="z"){//pause has no tune
+    switch(Tstate){//for tunes
       case 0:
        ch=ch+",";
        break;
@@ -117,24 +117,25 @@ var toabcnote = (ch) => {
   N = 2     x 2( 1-(---)    ), n=Dstate%10, M=Math.floor(Dstate/10).
                      2
   */
-  ch=ch+numtostr(Math.pow(2,Dstate%10-4)*(1-Math.pow(1/2,Math.floor(Dstate/10)+1)));
+	//for duration
+  ch=ch+"*"+numtostr(Math.pow(2,Dstate%10-4)*(1-Math.pow(1/2,Math.floor(Dstate/10)+1)));
   return ch;
 };
 
-var insert = (str,md) => {
-  var InsBef=mvpos(1);
-  if(InsBef!=CrtPos){
-    if(abcstr[InsBef-1]=="\n")
+var insert = (str,md) => {//insert string in the right position. mode=1 for notations not occupying a position
+  var InsBef=mvpos(1);//insert before
+  if(InsBef!=CrtPos){//not the last note
+    if(abcstr[InsBef-1]=="\n")//if on the position before a "\n", insert before "\n"
       InsBef--;
     abcstr=abcstr.substring(0,InsBef)+(md!=1?"$":"")+str+abcstr.substring(InsBef);
   }else{
-    abcstr=abcstr+(md!=1?"$":"")+str;
+    abcstr=abcstr+(md!=1?"$":"")+str;//append
   }
   CrtPos=(md!=1)?mvpos(1):CrtPos;
   console.log(abcstr);
 };
 
-var insertch = (str) => {
+var insertch = (str) => {//insert for chord
   for(var i=mvpos(1)+2;i<abcstr.length;i++){
     if(abcstr[i]=="]"){
       abcstr=abcstr.substring(0,i)+str+abcstr.substring(i);
@@ -143,12 +144,12 @@ var insertch = (str) => {
   }
 };
 
-var rmsmb = (str) => {
-  return str.replace(/[\\$]|[\\#]/g,"");
+var rmsmb = (str) => {//remove symbols should not be in the final abcstring
+  return str.replace(/[\\*][\\$]|[\\#]/g,"");
 };
 
 
-var checkinput = () => {
+var checkinput = () => {//if input tags are focused, turn off key events
  let myArray = Array.from(document.getElementsByTagName("input"));
   if(myArray.includes(document.activeElement))
     return true;
@@ -159,31 +160,31 @@ var checkinput = () => {
 var key = () => { // only keypress can tell if "shift" is pressed at the same time
   if(checkinput()) return;
 	switch(event.keyCode){
-		case 44:
+		case 44://"<"
 				Dstate=(Dstate%10==0)?8:Dstate-1;
 			break;
-		case 46:
+		case 46://">"
 				Dstate=(Dstate%10==8)?0:Dstate+1;
 			break;
-		case 60:
+		case 60://"shift+>"
 			if((Math.floor(Dstate/10))!=0)
 				Dstate=Dstate-10;
 			break;
-		case 62:
+		case 62://"shift+<"
 			Dstate=Dstate+10;
 			break;
 	// ----------Change Dstate-----------
-    case 63: // for chord mode
-		case 47:
+    case 63://"shift+?" for chord mode
+		case 47://"?"
 			Tstate=(Tstate==3)?0:Tstate+1;
 			break;
 	// ----------Change Tstate-----------
-		case 113:
+		case 113://"Q"
       if((CrtPos!=0)&&(abcstr[CrtPos-1]!="\n")){
-        var DelEnd=mvpos(1);
+        var DelEnd=mvpos(1);//delete end
         var Latter="";
-        if(DelEnd!=CrtPos){
-          if(abcstr[DelEnd-1]=="\n")
+        if(DelEnd!=CrtPos){//if not the last note
+          if(abcstr[DelEnd-1]=="\n")//if on the position before a "\n", keep "\n" in Latter
             DelEnd--;
           Latter=abcstr.substring(DelEnd);
         }
@@ -194,35 +195,35 @@ var key = () => { // only keypress can tell if "shift" is pressed at the same ti
       }
 			break;
 	// ----------Delete-----------------
-    case 122:
+    case 122://"Z"
       insert(toabcnote("C"),0);
       break;
-    case 120:
+    case 120://"X"
       insert(toabcnote("D"),0);
       break;
-    case 99:
+    case 99://"C"
       insert(toabcnote("E"),0);
       break;
-    case 118:
+    case 118://"V"
       insert(toabcnote("F"),0);
       break;
-    case 98:
+    case 98://"B"
       insert(toabcnote("G"),0);
       break;
-    case 110:
+    case 110://"N"
       insert(toabcnote("A"),0);
       break;
-    case 109:
+    case 109://"M"
       insert(toabcnote("B"),0);
       break;
   // ----------Insert Note------------
-		case 97:
+		case 115://"S"
       if(abcstr[((mvpos(1)!=CrtPos)?mvpos(1):abcstr.length)-1]!=" ")
         insert(" ",1);
       break;
-  // ----------Break Notes------------
-    case 115:
-      var RmBef=mvpos(1);
+  // ----------Seperate Notes---------
+    case 97://"A"
+      var RmBef=mvpos(1);//remove before
       if(abcstr[((RmBef!=CrtPos)?RmBef:abcstr.length)-1]==" "){
         if(RmBef==CrtPos){
           abcstr=abcstr.substring(0,abcstr.length-1);
@@ -232,66 +233,68 @@ var key = () => { // only keypress can tell if "shift" is pressed at the same ti
       }
       break;
   // ----------Assemble Notes---------
-    case 100:
-      if(Dstate%10!=8){
+    case 100://"D"
+      if(Dstate%10!=8){//Pause with duration of 8 is illegal
         insert(toabcnote("z"),0);
       }else{
         alert("Pause with duration of 8 is illegal.");
       }
       break;
   // ----------Insert Pause-----------
-    case 92:
+    case 92://"|"
       insert("|",0);
       break;
   // ----------Insert Bar-------------
-    case 93: // ] : # 
+    case 93://"]" for #
       if(CrtPos!=0){
-        if(abcstr[CrtPos+2]!="^"){
+        if(abcstr[CrtPos+2]!="^"){//only allow 2 #s
           if(abcstr[CrtPos+1]!="_"){
            abcstr=abcstr.substring(0,CrtPos+1)+"^"+abcstr.substring(CrtPos+1);
-         }else{
+         }else{//if b exists, delete one b
            abcstr=abcstr.substring(0,CrtPos+1)+abcstr.substring(CrtPos+2);
          }
         }
       }
       break;
-    case 91: // [ : b
+    case 91://"[" for b
       if(CrtPos!=0){
-        if(abcstr[CrtPos+2]!="_"){
+        if(abcstr[CrtPos+2]!="_"){//only allow 2 bs
           if(abcstr[CrtPos+1]!="^"){
             abcstr=abcstr.substring(0,CrtPos+1)+"_"+abcstr.substring(CrtPos+1);
-          }else{
+          }else{//if # exists, delete one #
             abcstr=abcstr.substring(0,CrtPos+1)+abcstr.substring(CrtPos+2);
          }
         }
       }
       break;
   // ----------Accidental-------------
-    case 90:
+    case 90://"shift+Z"
       insertch(toabcnote("C"));
       break;
-    case 88:
+    case 88://"shift+X"
       insertch(toabcnote("D"));
       break;
-    case 67:
+    case 67://"shift+C"
       insertch(toabcnote("E"));
       break;
-    case 86:
+    case 86://"shift+V"
       insertch(toabcnote("F"));
       break;
-    case 66:
+    case 66://"shift+B"
       insertch(toabcnote("G"));
       break;
-    case 78:
+    case 78://"shift+N"
       insertch(toabcnote("A"));
       break;
-    case 77:
+    case 77://"shift+M"
       insertch(toabcnote("B"));
       break;
   // ----------Chord Mode-------------
-    case 13:
-      insert("\n$",1);
-      CrtPos=mvpos(1);
+    case 13://"enter"
+			if(CrtPos!=0 && abcstr[CrtPos-1]!="\n"){//ABCJS doesn't allow 2 "\n"s in series
+      	insert("\n$",1);
+      	CrtPos=mvpos(1);
+			}
       break;
   // ----------New Line---------------
     default:
@@ -303,29 +306,46 @@ var key = () => { // only keypress can tell if "shift" is pressed at the same ti
 };
 
 var move = () => { // some keys can't be detected in keypress
-  if(checkinput()) return;
-	if(event.keyCode==37){
+  if(checkinput()) return;//if inpus tags are focus, turn off key events
+	//not using switch for speed(avoid looking up table)
+	if(event.keyCode==37){//"left"
 		CrtPos=mvpos(0);
 	}
-	if(event.keyCode==39){
+	if(event.keyCode==39){//"right"
 		CrtPos=mvpos(1);
 	}
-  if(event.keyCode==38){
+  if(event.keyCode==38){//"up"
     CrtPos=mvpos(2);
   }
-  if(event.keyCode==40){
+  if(event.keyCode==40){//"down"
     CrtPos=mvpos(3);
   }
-  if(event.keyCode==8){
-    var NxtPos=mvpos(0);
-    if(abcstr[CrtPos-1]=="\n"){
-      abcstr=abcstr.substring(0,CrtPos-1)+abcstr.substring(CrtPos+1);
-      CrtPos=NxtPos;
-    }
+  if(event.keyCode==8){//"backspace"
+		if(CrtPos!=0){//if not the start of abcstring
+			if(abcstr[CrtPos-1]!="\n"){//deleting notes
+				var DelEnd=mvpos(1);//delete end
+				var Latter="";
+				if(DelEnd!=CrtPos){//if not the last note
+					if(abcstr[DelEnd-1]=="\n")//if on the position before a "\n", keep "\n" in Latter
+						DelEnd--;
+					Latter=abcstr.substring(DelEnd);
+				}
+				abcstr=abcstr.substring(0,CrtPos)+Latter;
+				CrtPos=mvpos(0);
+			}else{//deleting "\n"
+				var NxtPos=mvpos(0);//next position
+				if(abcstr[CrtPos-1]=="\n"){
+					abcstr=abcstr.substring(0,CrtPos-1)+abcstr.substring(CrtPos+1);
+					CrtPos=NxtPos;
+				}
+			}
+		}
+		console.log(CrtPos);
+		console.log(abcstr);
     print();
   }
-  if(event.keyCode==16){
-    var InsBef=mvpos(1);
+  if(event.keyCode==16){//"shift" for chord mode on
+    var InsBef=mvpos(1);//insert before
     if((InsBef==CrtPos)||(abcstr[InsBef+1]!="[")){
       insert("$[]",1);
     }
@@ -333,10 +353,10 @@ var move = () => { // some keys can't be detected in keypress
 	console.log(CrtPos);
 };
 
-var chord = () => {
+var chord = () => {//keyup event for chord mode
   if(checkinput()) return;
-  if(event.keyCode==16){
-    if(abcstr.substr(mvpos(1),3)==="$[]"){
+  if(event.keyCode==16){//"shift" for chord mode off
+    if(abcstr.substr(mvpos(1),3)==="$[]"){//if no notes are inserted
       abcstr=abcstr.substring(0,mvpos(1))+abcstr.substring(mvpos(1)+3);
     }else{
       abcstr=abcstr.substring(0,mvpos(1)+1)+"#"+abcstr.substring(mvpos(1)+1);
@@ -345,7 +365,7 @@ var chord = () => {
   }
 };
 
-var btn = (a) => {
+var btn = (a) => {//buttons for notes
   insert(toabcnote(a.id),0);
   print();
 };

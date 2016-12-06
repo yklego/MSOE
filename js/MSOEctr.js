7b3ec643f513dba73d6d90b1f89bd4f94a61a4fa
DanTNC <fad11204@yahoo.com.tw>
2016-12-07 00:22:07 +0800

    Update MSOEctr.js

--- js/MSOEctr.js
+++ js/MSOEctr.js
@@ -320,12 +320,14 @@ var key = () => { // only keypress can tell if "shift" is pressed at the same ti
 			break;
 	// ----------Change Dstate-----------
     case 63://"shift+?" for chord mode
-		case 47://"?"
-			Tstate=(Tstate==3)?0:Tstate+1;
-			break;
+    case 47://"?"
+      Tstate=(Tstate==3)?0:Tstate+1;
+      document.getElementById("octave").innerHTML(Tstate+3);
+      break;
     case 34://"shift+'" for chord mode
     case 39://"'" 
       Tstate=(Tstate==0)?3:Tstate-1;
+      document.getElementById("octave").innerHTML(Tstate+3);
       break;
 	// ----------Change Tstate-----------
     case 122://"Z"

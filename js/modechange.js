956e5fdf84531547edd542b3770fa873000159ad
yihho <oo651oo651@gmail.com>
2017-01-04 15:33:32 +0800

    add mode

---
+++ js/modechange.js
@@ -0,0 +1,11 @@
+var edit_mode = () =>{
+  $(".left").show();
+  $(".panel-group").show();
+  $(".edit_line").show();
+}
+var preview_mode = () =>{
+  $(".left").hide();
+  $(".panel-group").hide();
+  $(".edit_line").hide();
+}
+
